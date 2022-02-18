374b8b205266cc11c5c99dc9604270e9a2a19037
adamnussbaum1 <adam.nussbaum@kellogg.northwestern.edu>
2022-02-18 19:06:55 +0000

    created new place

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,2 +1,13 @@
 class PlacesController < ApplicationController
+
+    def new
+        @place = Place.new
+    end
+
+    def create
+        @place = Place.new(params["place"])
+        @place.save
+        redirect_to "/places"
+    end
+
 end
